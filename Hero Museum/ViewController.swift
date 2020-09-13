dd317641dcec962f769a481756dbd1e4ff0bb825
Ameer Muhammed <ami.mullan@gmail.com>
2020-09-13 22:54:04 +0530

    init-v1

--- Hero Museum/ViewController.swift
+++ Hero Museum/ViewController.swift
@@ -11,65 +11,71 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let imageRef = ARReferenceImage.referenceImages(inGroupNamed: "Heroes", bundle: Bundle.main)
+        {
+            configuration.trackingImages = imageRef
+            configuration.maximumNumberOfTrackedImages = 2
+        }
+        
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            plane.firstMaterial?.diffuse.contents = UIColor(white: 1, alpha: 0.6)
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -Float.pi/2
+            
+            if(imageAnchor.referenceImage.name=="wallace")
+            {
+                if let heroIcon = SCNScene(named: "art.scnassets/wallace_sword.scn")
+                {
+                    if let heroIconNode = heroIcon.rootNode.childNodes.first {
+                        heroIconNode.eulerAngles.x = Float.pi/2
+                        planeNode.addChildNode(heroIconNode)
+                    }
+                }
+            }
+            else if(imageAnchor.referenceImage.name=="alexander") {
+                if let heroIcon = SCNScene(named: "art.scnassets/horse.scn")
+                {
+                    if let heroIconNode = heroIcon.rootNode.childNodes.first {
+                        heroIconNode.eulerAngles.x = Float.pi/2
+                        planeNode.addChildNode(heroIconNode)
+                    }
+                }
+            }
+            
+            node.addChildNode(planeNode)
+            
+            
+        }
+        
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
